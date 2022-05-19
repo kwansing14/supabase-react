8ca3f2d6310497893cc740f581c19a839e09317b
kwansing14 <kwansing@hotmail.sg>
2022-05-19 19:28:48 +0800

    create and get working

--- src/App.js
+++ src/App.js
@@ -6,7 +6,46 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [post, setPost] = useState({ title: '', content: '' });
   const { title, content } = [post];
-  return <div className='App'></div>;
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const { data } = await supabase.from('posts').select();
+    console.log('data: ', data);
+    setPosts(data);
+  };
+
+  const createPost = async () => {
+    await supabase
+      .from('posts')
+      .insert([{ title: post.title, content: post.content }]);
+    setPost({ title: '', content: '' });
+    fetchPosts();
+  };
+
+  return (
+    <div className='App'>
+      <input
+        placeholder='Title'
+        value={title}
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
+      />
+      <input
+        placeholder='Content'
+        value={content}
+        onChange={(e) => setPost({ ...post, content: e.target.value })}
+      />
+      <button onClick={createPost}>Create Post</button>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
